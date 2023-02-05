c47f6a49347c78ceb67d3517de4a39bb4f917bee
NathanStanding <standingtall1000@gmail.com>
2023-02-04 22:11:30 -0700

    simplified viewGauge

--- src/components/viewGuage.js
+++ src/components/viewGuage.js
@@ -6,51 +6,38 @@ function Views ({views}) {
 
         // totalViews = 100001
         var impactLevel = 'nothing'
- 
-        if (totalViews<=10000) {
-            impactLevel = .033
-        } else if (totalViews>10000 && totalViews<=20000) {
-            impactLevel = .066;
-        } else if (totalViews>20000 && totalViews<=30000) {
-            impactLevel = .099;
-        } else if (totalViews>30000 && totalViews<=40000) {
-            impactLevel = .132;
-        } else if (totalViews>40000 && totalViews<=50000) {
-            impactLevel = .165;
-        } else if (totalViews>50000 && totalViews<=60000) {
-            impactLevel = .198;
-        } else if (totalViews>60000 && totalViews<=70000) {
-            impactLevel = .231;
-        } else if (totalViews>70000 && totalViews<=80000) {
-            impactLevel = .264;
-        } else if (totalViews>80000 && totalViews<=90000) {
-            impactLevel = .297;
-        } else if (totalViews>90000 && totalViews<100000) {
-            impactLevel = .30;
-        } else if (totalViews>=100000 && totalViews<=200000) {
-            impactLevel = .396;
-        } else if (totalViews>200000 && totalViews<=300000) {
-            impactLevel = .429;
-        } else if (totalViews>300000 && totalViews<=400000) {
-            impactLevel = .462;
-        } else if (totalViews>400000 && totalViews<=500000) {
-            impactLevel = .495;
-        } else if (totalViews>500000 && totalViews<=600000) {
-            impactLevel = .528;
-        } else if (totalViews>600000 && totalViews<=700000) {
-            impactLevel = .561;
-        } else if (totalViews>700000 && totalViews<=800000) {
-            impactLevel = .594;
-        } else if (totalViews>800000 && totalViews<=900000) {
-            impactLevel = .627;
-        } else if (totalViews>900000 && totalViews<=1000000) {
-            impactLevel = .66;
+        var number1 = 'nothing'
+        var number2 = 'nothing'
+        var number3 = 'nothing'
+        if (totalViews <=100000){
+            number1 = (totalViews/100000)*10
+            impactLevel = 0 + (0.033 * number1)
+        } else if (totalViews>100000 && totalViews<=1000000) {
+            number2 = (totalViews/1000000)*10
+            impactLevel = .33 + (0.033 * number2)
+        } else if (totalViews>1000000 && totalViews<=10000000){
+            number3 = (totalViews/10000000)*10
+            impactLevel = .66 + (0.033 * number3)
         } else {
-            impactLevel = .75;
+            impactLevel = 1
         }
+        // 200000/ 1000000
+        // .2 
 
-        // 10000/1000000 *.033
+
+
+        // 20000/100000
+        // 1/10
+        // *10
+        // .33 + (0.033 * 2)
+
+        
+
+        console.log(impactLevel)
+        // 10000/1000000 * 3.3
         // 100,000/1000000 *.33 - nope
+
+        
        
         // console.log('impactLevel is: ' + impactLevel)
         // console.log(impactLevel)
